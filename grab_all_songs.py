9ccb307e6db2591034cb870a3a5f793cdb3cdc14
Wyatt Hall <hallwyatt3@gmail.com>
2021-02-24 16:10:21 -0700

    removed main(); made class getSongs

--- grab_all_songs.py
+++ grab_all_songs.py
@@ -5,38 +5,35 @@ from bs4 import BeautifulSoup as bs
 
 #the purpose of this code is to grab all the songs that are available for a given artist on az lyrics
 
-def produce_url(artist):
+class getSongs:
+    def __init__(self, artist):
+        self.artist = artist
 
-    def remove_special_chars(name):
-        name = re.sub(r'[^A-Za-z0-9]+', '', name)
-        return name.lower()
+    def produce_url(artist):
 
-    artist = remove_special_chars(artist)
-    return f'https://www.azlyrics.com/{artist[0]}/{artist}.html'
+        def remove_special_chars(name):
+            name = re.sub(r'[^A-Za-z0-9]+', '', name)
+            return name.lower()
 
-def fetch_url(url):
-    az = requests.get(url)
-    soup = bs(az.text, 'html.parser')
-    return soup
+        artist = remove_special_chars(artist)
+        return f'https://www.azlyrics.com/{artist[0]}/{artist}.html'
 
-def get_song_list(soup):
-    #this function takes the soup fetched by the fetch_url function and 
-    # returns a list of songs by the artist
-    global all_songs_list
-    songs = soup.find_all('a')
-    songs = songs[34:-8]
-    all_songs_list = []
-    for i in songs:
-        all_songs_list.append(str(i.get_text()))
+    def fetch_url(url):
+        az = requests.get(url)
+        soup = bs(az.text, 'html.parser')
+        return soup
 
-    return all_songs_list
+    def get_song_list(soup):
+        #this function takes the soup fetched by the fetch_url function and 
+        # returns a list of songs by the artist
+        global all_songs_list
+        songs = soup.find_all('a')
+        songs = songs[34:-8]
+        all_songs_list = []
+        for i in songs:
+            all_songs_list.append(str(i.get_text()))
 
-def main(artist):
-    url = produce_url(artist)
-    soup = fetch_url(url)
-    return get_song_list(soup)
-
-main(input('What artist are we searching for today? '))
+        return all_songs_list
 
 print(all_songs_list)
 print(len(all_songs_list))
