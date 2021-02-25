9cbb13a53c1a954445cd8b7256fc53262a229898
Wyatt Hall <hallwyatt3@gmail.com>
2021-02-24 18:43:04 -0700

    built a bad script for scoring all songs by artist

--- rank_song.py
+++ rank_song.py
@@ -2,6 +2,14 @@
 #to find the worst song
 
 from grab_all_songs import getSongs
+from scraping import rankLyrics
 
-gs = getSongs('frank ocean')
-print(gs.get_song_list())
+artist = 'frank ocean'
+
+#this is hella slow. Try to find a way to make it so it doesn't have to call the website every single time
+#may have to reorganize some code... let's see
+gs = getSongs(artist)
+total_songs = gs.get_song_list()
+for i in total_songs:
+    rl = rankLyrics(artist, i)
+    print(rl.score_bw())
